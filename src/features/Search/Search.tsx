e640c79b8903ada853375d86adb89da337eff79a
Dovgopol Sergii <ghostnord@gmail.com>
2024-07-01 10:04:50 +0300

    upd search

--- src/features/Search/Search.tsx
+++ src/features/Search/Search.tsx
@@ -1,12 +1,60 @@
 import SearchIcon from "@/assets/search.svg";
-import Dropdown from "@/components/Dropdown/Dropdown";
+import Button from "@/components/Button/Button";
 import Icon from "@/components/Icon/Icon";
 import Input from "@/components/Input/Input";
+import Portal from "@/components/Portal/Portal";
+import { cn } from "@/lib/utils";
 import { useRouter } from "next/navigation";
-import { ChangeEvent, useCallback } from "react";
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from "react";
 
 function Search() {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  const toggleSearch = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    setIsOpen((prev) => !prev);
+  }, []);
+
+  const closeNav = useCallback(() => {
+    setIsOpen(false);
+  }, []);
+
+  const handleEscapeKey = useCallback(
+    (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        closeNav();
+      }
+    },
+    [closeNav]
+  );
+
+  const handleClickOutside = useCallback(
+    (e: MouseEvent) => {
+      if (
+        menuRef.current &&
+        e.target instanceof Node &&
+        !menuRef.current.contains(e.target) &&
+        !(e.target as HTMLElement).closest("#buttonSearch")
+      ) {
+        closeNav();
+      }
+    },
+    [closeNav]
+  );
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("keydown", handleEscapeKey);
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("keydown", handleEscapeKey);
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen, handleEscapeKey, handleClickOutside]);
 
   const handleSearchChange = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -19,19 +67,29 @@ function Search() {
 
   return (
     <div>
-      <Dropdown
-        className=" w-[300px]"
-        position="bottomStart"
-        trigger={
-          <Icon
-            className="duration-200 hover-hover:hover:scale-[1.2] hover-none:active:scale-[1.2] active:scale-[1]"
-            width={16}
-            height={16}
-            Svg={SearchIcon}
-          />
-        }
+      <Button
+        id="buttonSearch"
+        title="Search"
+        variant="clear"
+        bgColor="clear"
+        onClick={toggleSearch}
       >
-        <div className=" p-2">
+        <Icon
+          className="duration-200 hover-hover:hover:scale-[1.2] hover-none:active:scale-[1.2] active:scale-[1]"
+          width={16}
+          height={16}
+          Svg={SearchIcon}
+        />
+      </Button>
+
+      <Portal>
+        <div
+          ref={menuRef}
+          className={cn(
+            " duration-300 flex absolute items-center justify-center left-0 top-[73px] bg-white w-screen px-10 pt-3  z-40 ",
+            isOpen ? " top-[73px]" : " top-[-100%]"
+          )}
+        >
           <Input
             onChange={handleSearchChange}
             label="Search"
@@ -40,7 +98,7 @@ function Search() {
             type="search"
           />
         </div>
-      </Dropdown>
+      </Portal>
     </div>
   );
 }
