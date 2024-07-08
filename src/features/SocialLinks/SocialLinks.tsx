ec43d8c301dc4aee07cdb661fb73a0d156a4a216
Dovgopol Sergii <ghostnord@gmail.com>
2024-07-08 12:46:19 +0300

    upd Svg compr

--- src/features/SocialLinks/SocialLinks.tsx
+++ src/features/SocialLinks/SocialLinks.tsx
@@ -1,34 +1,47 @@
 import AppLink from "@/components/AppLink/AppLink";
+import Icon from "@/components/Icon/Icon";
+import LinkedinIcon from "@/assets/linkedin.svg";
+import FacebookIcon from "@/assets/facebook.svg";
+import InstagramIcon from "@/assets/instagram.svg";
+import SkypeIcon from "@/assets/skype.svg";
+import TwitterIcon from "@/assets/twitter.svg";
+import PinterestIcon from "@/assets/pinterest.svg";
 
 const socialLinks = [
   {
     id: "linkedin",
     title: "Linkedin",
+    icon: LinkedinIcon,
     href: "#",
   },
   {
     id: "facebook",
     title: "Facebook",
+    icon: FacebookIcon,
     href: "#",
   },
   {
     id: "instagram",
     title: "Instagram",
+    icon: InstagramIcon,
     href: "#",
   },
   {
     id: "skype",
     title: "Skype",
+    icon: SkypeIcon,
     href: "#",
   },
   {
     id: "twitter",
     title: "Twitter",
+    icon: TwitterIcon,
     href: "#",
   },
   {
     id: "pinterest",
     title: "Pinterest",
+    icon: PinterestIcon,
     href: "#",
   },
 ];
@@ -43,9 +56,7 @@ function SocialLinks() {
             title={link.title}
             href={link.href}
           >
-            <svg width={18} height={18} className={"  text-white "}>
-              <use href={`/svg/social-links.svg#${link.id}`} />
-            </svg>
+            <Icon width={18} height={18} Svg={link.icon} />
           </AppLink>
         </li>
       ))}
