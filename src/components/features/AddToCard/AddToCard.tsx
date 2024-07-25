9a5c2acdacc84370a473e032788d3cd991b06c0b
Dovgopol Sergii <ghostnord@gmail.com>
2024-07-25 11:57:40 +0300

    Refactor/AddToCard

--- src/components/features/AddToCard/AddToCard.tsx
+++ src/components/features/AddToCard/AddToCard.tsx
@@ -8,13 +8,21 @@ import { addItemToCart } from "@/lib/cart";
 import type { Product } from "@/types/product";
 import { useFormState, useFormStatus } from "react-dom";
 
+function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <Button disabled={pending} type="submit" variant="filled">
+      Add to cart
+    </Button>
+  );
+}
+
 interface Props {
   product: Product;
 }
 
 function AddToCard({ product }: Props) {
   const [message, formAction] = useFormState(addItemToCart, null);
-  const { pending } = useFormStatus();
 
   const [quantity, setQuantity] = useState(1);
 
@@ -36,9 +44,7 @@ function AddToCard({ product }: Props) {
       />
       <form action={action}>
         <div className=" relative flex gap-4 mt-9 lg:flex-col">
-          <Button disabled={pending} type="submit" variant="filled">
-            Add to cart
-          </Button>
+          <SubmitButton />
           <Button type="button" variant="filled" bgColor="white">
             Save to favorites
           </Button>
