8f730622574726a38c19497711284e9400a6220d
ValpSerg <Ghostnord@gmail.com>
2024-08-08 21:04:39 +0300

    Tests/ clear lines

--- src/components/features/GoogleButton/GoogleButton.spec.tsx
+++ src/components/features/GoogleButton/GoogleButton.spec.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import GoogleButton from "./GoogleButton";
 import { signIn } from "next-auth/react";
