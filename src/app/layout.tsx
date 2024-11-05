32c16492219c0a52bc79f21e98b0cc75aa10f035
ValpSerg <Ghostnord@gmail.com>
2024-11-05 16:35:27 +0200

    Refactor / isMobile to Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
-import { headers } from "next/headers";
 import localFont from "next/font/local";
 import Header from "@/components/features/Header/Header";
 import Footer from "@/components/features/Footer/Footer";
 import Banner from "@/components/features/Banner/Banner";
 import Providers from "@/components/shared/Providers/Providers";
-import { isMobile } from "@/lib/isMobile/isMobile";
 import "./globals.css";
 
 const satoshi = localFont({
@@ -34,15 +32,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const userAgent = headers().get("user-agent") || "";
-  const mobileCheck = isMobile(userAgent);
-
   return (
     <html lang="en">
       <body className={`${satoshi.variable} ${clashDisplay.variable}`}>
         <Providers>
           <Banner />
-          <Header isMobile={mobileCheck} />
+          <Header />
           {children}
           <Footer />
         </Providers>
