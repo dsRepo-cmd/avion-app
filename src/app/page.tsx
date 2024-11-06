503b92c0599232fb9cb8edc20546747b167ed5d2
ValpSerg <Ghostnord@gmail.com>
2024-11-06 11:00:17 +0200

    Add / ListingsSkeleton to profile page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import dynamic from "next/dynamic";
 import Features from "@/components/features/Features/Features";
 import JoinUs from "@/components/features/JoinUs/JoinUs";
 import Page from "@/components/shared/Page/Page";
+import ListingsSkeleton from "@/components/shared/ListingsSkeleton/ListingsSkeleton";
 import Hero from "./hero";
 import Info from "./info";
-import ListingsSkeleton from "@/components/shared/ListingsSkeleton/ListingsSkeleton";
 
 const NewProductListings = dynamic(
   () => import("@/components/features/NewProductListings/NewProductListings"),
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,28 @@
 import Image from "next/image";
+import dynamic from "next/dynamic";
 import { getServerSession } from "next-auth";
 import { authConfig } from "@/configs/auth";
-import PopularProductListings from "@/components/features/PopularProductListings/PopularProductListings";
 import Container from "@/components/shared/Container/Container";
 import Page from "@/components/shared/Page/Page";
 import Typography from "@/components/shared/Typography/Typography";
+import ListingsSkeleton from "@/components/shared/ListingsSkeleton/ListingsSkeleton";
 
+const PopularProductListings = dynamic(
+  () =>
+    import(
+      "@/components/features/PopularProductListings/PopularProductListings"
+    ),
+  {
+    ssr: false,
+    loading: () => <ListingsSkeleton />,
+  }
+);
 async function Profile() {
   const session = await getServerSession(authConfig);
 
   return (
     <Page>
-      <Container>
+      <Container bgColor={"light"}>
         <div className=" flex justify-between w-full lg:flex-col gap-10">
           <Typography fontFamily="secondary" size="32px" tag="h2">
             Welcome, {session?.user?.name}
